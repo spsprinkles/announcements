dfc7cca4209b58518de8a4f0b02841057132e131
Gunjan Datta <gudatta@microsoft.com>
2023-07-11 11:43:12 -0400

    Incremented the version.

--- src/strings.ts
+++ src/strings.ts
@@ -24,6 +24,6 @@ const Strings = {
     ProjectName: "Announcements",
     ProjectDescription: "Displays announcements at the top of a modern page.",
     SourceUrl: ContextInfo.webServerRelativeUrl,
-    Version: "0.1"
+    Version: "0.0.0.2"
 };
 export default Strings;
